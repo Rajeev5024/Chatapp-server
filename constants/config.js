3e069596d25038a6de46fcb6a8cdb8a81b238a1a
Rajeev5024 <135305676+Rajeev5024@users.noreply.github.com>
2024-07-26 21:06:33 +0530

    Update config.js
    fixed cors issue

--- constants/config.js
+++ constants/config.js
@@ -4,12 +4,12 @@
 // 	// 	process.env.CLIENT_URL,
 	
 // 	 ],
-const corsOptions={
-	
-		origin:"*",
-		credentials:true,
-	
-}
+const corsOptions = {
+  origin: true, // This will allow requests from any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // if you need to send cookies
+};
 
 const CHATAPP_TOKEN="Chatapp-token";
 
