04b104810f9ebe27cd53ec8f53e8c72bc406d454
Rajeev5024 <rajeevkumarranjan880@gmail.com>
2024-07-30 16:53:02 +0530

    test

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // origin:[
 // 	"http://127.0.0.1:5173",
 // 	"http://localhost:4173",
@@ -10,20 +10,6 @@ const corsOptions={
 		origin : "*",
 		credentials:true,
 }
-=======
-// [
-// 	// 	"http://127.0.0.1:5173",
-// 	// 	"http://localhost:4173",
-// 	// 	process.env.CLIENT_URL,
-	
-// 	 ],
-const corsOptions = {
-  origin: true, // This will allow requests from any origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // if you need to send cookies
-};
->>>>>>> be72c45fc71b846d766dcad0648671c8fe2a9794
 
 const CHATAPP_TOKEN="Chatapp-token";
 
